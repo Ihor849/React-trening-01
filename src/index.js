6379b35a9c671e8ce652ddf65b3e0cce0d0c9b11
Ihor849 <333marchelo333@gmail.com>
2023-03-27 13:26:57 +0300

    проба1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+// import { App } from 'components/App';
+// import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+const element = React.createElement('div', {
+  a: 5,
+  b: 10,
+  children: ['Привет мир'],
+});
+console.log(element);
+ReactDOM.render(element, document.querySelector('#root'));
