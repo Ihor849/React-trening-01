632ac51786f55b7a64ab4939675b96d8eecd110b
Ihor849 <333marchelo333@gmail.com>
2023-04-20 23:36:43 +0300

    тема1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,21 @@
-// import user from 'components/json/user.json'
-
+import { Alert} from '../../components/Alert'
 
 
 export const App = () => {
   return (
-    <div>
-      <App/>
-    </div>
-  )
-}
+    <>
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+    </>
+  );
+};
